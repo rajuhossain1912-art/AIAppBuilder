0504b77d10a4c6fffa30be4d67146eb0958c1bad
rajuhossain1912-art <rajuhossain1912@gmail.com>
2026-09-12 22:11:40 +0600

    Export app intent template selection

--- agent/templates/__init__.py
+++ agent/templates/__init__.py
@@ -1,5 +1,11 @@
 """Reusable Android project templates and template selection helpers."""
 
-from .android_template import AndroidTemplateCatalog, AndroidProjectSpec
+from .android_template import AndroidProjectSpec, AndroidTemplateCatalog
+from .app_intent import AppIntentClassifier, TemplateDecision
 
-__all__ = ["AndroidTemplateCatalog", "AndroidProjectSpec"]
+__all__ = [
+    "AndroidProjectSpec",
+    "AndroidTemplateCatalog",
+    "AppIntentClassifier",
+    "TemplateDecision",
+]
