7bfeec5709cccc1d984a31c65765b172f8aa9dce
rajuhossain1912-art <rajuhossain1912@gmail.com>
2026-09-12 22:11:18 +0600

    Add Android template package foundation

---
+++ agent/templates/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable Android project templates and template selection helpers."""
+
+from .android_template import AndroidTemplateCatalog, AndroidProjectSpec
+
+__all__ = ["AndroidTemplateCatalog", "AndroidProjectSpec"]
