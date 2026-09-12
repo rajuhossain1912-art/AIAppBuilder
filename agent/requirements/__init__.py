8f5aadf531ede77445f01b535c9327d88bef91ce
rajuhossain1912-art <rajuhossain1912@gmail.com>
2026-09-13 00:36:10 +0600

    Export capability classification primitives

--- agent/requirements/__init__.py
+++ agent/requirements/__init__.py
@@ -1,5 +1,11 @@
 """Requirement intake and normalization primitives."""
 
+from .capability_catalog import CapabilityMatch, classify_capabilities
 from .requirements_engine import RequirementEngine, RequirementSet
 
-__all__ = ["RequirementEngine", "RequirementSet"]
+__all__ = [
+    "CapabilityMatch",
+    "RequirementEngine",
+    "RequirementSet",
+    "classify_capabilities",
+]
