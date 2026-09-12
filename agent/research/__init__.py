8875a3c27d4cb6d82c68f5b25ecbfa277e01b4d6
rajuhossain1912-art <rajuhossain1912@gmail.com>
2026-09-12 22:04:26 +0600

    Add research package exports

---
+++ agent/research/__init__.py
@@ -0,0 +1,5 @@
+"""Evidence-based research primitives."""
+
+from .research_engine import ResearchEngine, ResearchError, ResearchRecord
+
+__all__ = ["ResearchEngine", "ResearchError", "ResearchRecord"]
